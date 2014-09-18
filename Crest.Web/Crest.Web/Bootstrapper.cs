f5fe217ab0b293e69b81381244ab268461455506
rfavillejr <rfavillejr@gmail.com>
2014-09-18 14:40:51 -0400

    Initial check in

---
+++ Crest.Web/Crest.Web/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Conventions;
+using Nancy.Session;
+using Nancy.TinyIoc;
+using System.Web.Routing;
+
+namespace Crest.Web
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        // The bootstrapper enables you to reconfigure the composition of the framework,
+        // by overriding the various methods and properties.
+        // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            this.Conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("Scripts"));
+            CookieBasedSessions.Enable(pipelines);
+            //RouteTable.Routes.MapHubs();
+        }
+    }
+}
---
+++ Crest.Web/Crest.Web/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace Crest.Web.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
