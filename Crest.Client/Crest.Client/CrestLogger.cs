b9c1d9768c20bf8f0a5db6d2f5d4fab8d6a86de2
rfavillejr <rfavillejr@gmail.com>
2014-09-19 09:33:36 -0400

    Created new client project, created simple hub and client

---
+++ Crest.Client/Crest.Client/CrestLogger.cs
@@ -0,0 +1,28 @@
+﻿using Crest.Client.Models;
+using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crest.Client
+{
+    public class CrestLogger
+    {
+        IHubProxy errorLogHubProxy;
+
+        public CrestLogger()
+        {
+            var hubConnection = new HubConnection("http://www.contoso.com/");
+            errorLogHubProxy = hubConnection.CreateHubProxy("ErrorLogHub");
+            //errorLogHubProxy.On<Error>("LogError", error => { });
+            hubConnection.Start().Wait();
+        }
+
+        public void LogError(Exception e)
+        {
+            errorLogHubProxy.Invoke("LogError", e);
+        }
+    }
+}
---
+++ Crest.Web/Crest.Web/ErrorLogHub.cs
@@ -0,0 +1,24 @@
+﻿using Crest.Web.Models;
+using Microsoft.AspNet.SignalR;
+using Dapper;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Crest.Web
+{
+	public class ErrorLogHub : Hub
+	{
+		public void LogError(Error errorToLog)
+		{
+            Error result = null;
+
+			//save info to DB
+            using(SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["CrestDB"].ConnectionString))
+            {
+               result = connection.Insert<Error>(errorToLog);
+            }
+
+            Clients.All.broadcastMessage(result);
+		}
+	}
+}
